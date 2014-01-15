61b88ff6a3f6e04be064e91adf8687d10c01c568
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-01-14 23:34:27 -0500

    added all but hex codes def

--- main.rb
+++ main.rb
@@ -1,45 +1,31 @@
-require_relative '../source'
-require 'pry'
+require_relative 'source'
 
-def valid_ip_addresses
-  # code should:
-  # - find strings that look like IP addresses
-  # - use rand(1..254) for first and last places in string, rand(0..254) for middle spots?
+def valid_ip_addresses(source_text)
+  source_text.scan(/\b(?:\d{1,3}\.){3}\d{1,3}\b/)
 end
 
-def valid_mit_ip_addresses
-  # code should:
-  # - find strings that look like IP addresses and start with 18.
+def valid_mit_ip_addresses(source_text)
+  source_text.scan(/18\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
 end
 
-def non_mit_ip_addresses
-  # code should:
-  # - find strings that look like IP addresses and don't start with 18.
+def non_mit_ip_addresses(source_text)
+  valid_ip_addresses(source_text) - valid_mit_ip_addresses(source_text)
 end
 
-def valid_phone_numbers(:source_text)
-  # code should:
-  # - find strings that look like phone numbers, whether or not they include a 1- at the very beginning.
-  # - exclude numbers where area code + town exchange exclude 3 digit strings: 100-700
+def valid_phone_numbers(source_text)
   source_text.scan(/[2-9]{1}\d{0,2}\-\d{0,3}\-?\d{4}/).uniq
-  binding.pry
 end
 
-def area_codes
-  # code should:
-  # - find strings that look like phone numbers, whether or not they include a 1- at the very beginning
-  # - exclude 3 digit number that starts with 1
+def area_codes(source_text)
+  source_text.scan(/(?<=\s)[2-9][0-8][0-9](?=-)/)
 end
 
-def email_addresses
-  # code should:
-  # - find strings that look like email addresses, containing string + @ + . + string.
+def email_addresses(source_text)
+  source_text.scan(/\w+@.+.com/)
 end
 
-def zip_codes
-  # code should:
-  # - find five digit strings with no other punctuation
-  # - exclude other digit strings
+def zip_codes(source_text)
+  source_text.scan(/\b\d{5}\b/)
 end
 
 def hex_colors
