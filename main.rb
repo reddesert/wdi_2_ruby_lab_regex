2f5114a85a3bcbbabc5578754254b64bb2ee03ff
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-01-14 22:44:40 -0500

    first. mostly just commented ramblings

--- main.rb
+++ main.rb
@@ -1,26 +1,48 @@
-require_relative 'source'
+require_relative '../source'
 require 'pry'
 
-def valid_ip_addresses 
-end 
-
-def valid_mit_ip_addresses 
+def valid_ip_addresses
+  # code should:
+  # - find strings that look like IP addresses
+  # - use rand(1..254) for first and last places in string, rand(0..254) for middle spots?
 end
 
-def non_mit_ip_addresses 
-end 
-
-def valid_phone_numbers 
+def valid_mit_ip_addresses
+  # code should:
+  # - find strings that look like IP addresses and start with 18.
 end
 
-def area_codes 
+def non_mit_ip_addresses
+  # code should:
+  # - find strings that look like IP addresses and don't start with 18.
 end
 
-def email_addresses 
+def valid_phone_numbers(:source_text)
+  # code should:
+  # - find strings that look like phone numbers, whether or not they include a 1- at the very beginning.
+  # - exclude numbers where area code + town exchange exclude 3 digit strings: 100-700
+  source_text.scan(/[2-9]{1}\d{0,2}\-\d{0,3}\-?\d{4}/).uniq
+  binding.pry
 end
 
-def zip_codes 
+def area_codes
+  # code should:
+  # - find strings that look like phone numbers, whether or not they include a 1- at the very beginning
+  # - exclude 3 digit number that starts with 1
 end
 
-def hex_colors 
+def email_addresses
+  # code should:
+  # - find strings that look like email addresses, containing string + @ + . + string.
+end
+
+def zip_codes
+  # code should:
+  # - find five digit strings with no other punctuation
+  # - exclude other digit strings
+end
+
+def hex_colors
+  # code should:
+  # - find strings that look like eithe
 end
